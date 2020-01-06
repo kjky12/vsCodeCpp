0016169e8e2d8b95e0b46762f406a6ac1b1532a5
Koo Jae Kyung <kjky123@naver.com>
2020-01-06 23:18:36 +0900

    최초 커밋!

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main()
+{
+
+    int a = 0;
+    int b = 1;
+
+    int c = 0;
+    int k = 0;
+    //scanf("%d", &c);
+    printf("%d", c);
+    
+    printf("гоюл");
+
+
+
+    return 0;
+}
